d6a6c1d797979f6de2dc5ee293d089352994e1f0
chanwon <clw8679@naver.com>
2025-08-20 15:16:19 +0900

    feat: ErrorCode 인터페이스 구현 (#5)

---
+++ src/main/java/ok/cherry/global/exception/error/ErrorCode.java
@@ -0,0 +1,10 @@
+package ok.cherry.global.exception.error;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+
+	String getMessage();
+	HttpStatus getStatus();
+	String getCode();
+}
