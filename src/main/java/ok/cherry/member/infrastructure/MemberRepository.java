9bd61f041f61d9162be4bce33f5cfa5e0da7c8a6
chanwon <clw8679@naver.com>
2025-08-21 18:10:14 +0900

    feat: MemberRepository 구현 (#10)

---
+++ src/main/java/ok/cherry/member/infrastructure/MemberRepository.java
@@ -0,0 +1,14 @@
+package ok.cherry.member.infrastructure;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ok.cherry.member.domain.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByProviderId(String providerId);
+}
