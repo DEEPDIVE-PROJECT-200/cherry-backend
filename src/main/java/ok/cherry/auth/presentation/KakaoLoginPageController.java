7477d394512c25bf70a63068bb504965e6cf5cc0
Jimin730 <dnddnd120@naver.com>
2025-08-22 18:07:53 +0900

    feat: 카카오 API 연동을 통한 토큰 요청 및 사용자 id 값 조회 기능 구현 (#10)

--- src/main/java/ok/cherry/auth/presentation/KakaoLoginPageController.java
+++ src/main/java/ok/cherry/auth/presentation/KakaoLoginPageController.java
@@ -9,12 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ok.cherry.auth.application.KakaoAuthService;
 
 @Slf4j
 @Controller
 @RequestMapping()
+@RequiredArgsConstructor
 public class KakaoLoginPageController {
+
+	private final KakaoAuthService kakaoAuthService;
 
 	@Value("${kakao.client_id}")
 	private String client_id;
@@ -35,6 +40,14 @@ public class KakaoLoginPageController {
 	@GetMapping("/callback")
 	public ResponseEntity<?> callback(@RequestParam("code") String code) {
 		log.info(code);
+		String accessToken = kakaoAuthService.getAccessToken(code);
+		String providerId = kakaoAuthService.getProviderId(accessToken); // providerId 까지는 무조건 가져올 수 있음
+
+		// todo: providerId 값으로 db에 사용자 정보가 있는지 조회
+
+		log.info("callback end");
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
+
+	// todo: 로그인 controller
 }
