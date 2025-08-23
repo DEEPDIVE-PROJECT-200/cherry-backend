8a681d0c7f9fad7adefc14b06b99ed9893ecd7f3
chanwon <clw8679@naver.com>
2025-08-23 14:29:24 +0900

    fix: security 설정 간소화 (#10)

--- src/main/java/ok/cherry/global/config/SecurityConfig.java
+++ src/main/java/ok/cherry/global/config/SecurityConfig.java
@@ -2,20 +2,14 @@ package ok.cherry.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
-import ok.cherry.auth.security.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -31,18 +25,14 @@ public class SecurityConfig {
 		"/v3/api-docs.yaml",
 		"/auth/**",
 		"/ws/**",
-		"/auth/**",
-		"/login/**",
 		"/static/**",
 		"/actuator/**",
 		"/metrics",
 	};
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http, DaoAuthenticationProvider daoAuthProvider) throws
-		Exception {
-		http.authenticationProvider(daoAuthProvider)
-			.csrf(AbstractHttpConfigurer::disable)
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.csrf(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.cors(Customizer.withDefaults())
@@ -51,28 +41,7 @@ public class SecurityConfig {
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(PERMIT_URL_ARRAY).permitAll()
 				// todo: 추후 인증 관련 권한 설정
-				.anyRequest().permitAll());
-		return http.build();
-	}
-
-	@Bean
-	public DaoAuthenticationProvider daoAuthenticationProvider(
-		CustomUserDetailsService userDetailsService,
-		PasswordEncoder passwordEncoder
-	) {
-		var provider = new DaoAuthenticationProvider(userDetailsService);
-		provider.setPasswordEncoder(passwordEncoder);
-		return provider;
-	}
-
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws
-		Exception {
-		return authenticationConfiguration.getAuthenticationManager();
+				.anyRequest().permitAll())
+			.build();
 	}
 }
