9774ab29418529475794d3181a02103a12785b76
Jimin730 <dnddnd120@naver.com>
2025-09-02 00:03:04 +0900

    chore: S3 의존성 변경 (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
     // S3
-    implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
+    implementation("io.awspring.cloud:spring-cloud-aws-starter-s3:3.4.0")
 
     // Thymeleaf Extensions
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
