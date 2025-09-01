2546b96297cffde2643dc73b85b084adbf6b633a
Jimin730 <dnddnd120@naver.com>
2025-09-01 14:49:34 +0900

    feat: Enumerated 어노테이션 추가 (#43)

--- src/main/java/ok/cherry/cart/domain/Cart.java
+++ src/main/java/ok/cherry/cart/domain/Cart.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -37,6 +39,7 @@ public class Cart {
 	@Column(nullable = false)
 	private BigDecimal price;
 
+	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private Color color;
 
