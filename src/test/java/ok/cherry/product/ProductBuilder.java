3699a6a3b091a6ecb6df3c0d66e5f26ef0bd170e
Jimin730 <dnddnd120@naver.com>
2025-09-04 13:20:03 +0900

    refactor: model 필드 제거 (#47)

--- src/test/java/ok/cherry/product/ProductBuilder.java
+++ src/test/java/ok/cherry/product/ProductBuilder.java
@@ -1,7 +1,7 @@
 package ok.cherry.product;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 import lombok.AccessLevel;
@@ -15,10 +15,9 @@ public class ProductBuilder {
 
 	public String name = "SONY WH-1000XM6";
 	public Brand brand = Brand.SONY;
-	public String model = "WH-1000XM6";
 	public List<Color> colors = List.of(Color.BLACK, Color.WHITE);
 	public BigDecimal dailyRentalPrice = BigDecimal.valueOf(10000);
-	public LocalDateTime launchedAt = LocalDateTime.of(2025, 5, 16, 0, 0, 0);
+	public LocalDate launchedAt = LocalDate.of(2025, 5, 16);
 
 	public static Product create() {
 		return builder().build();
@@ -29,7 +28,7 @@ public class ProductBuilder {
 	}
 
 	public Product build() {
-		return Product.create(name, brand, model, colors, dailyRentalPrice, launchedAt, null, null);
+		return Product.create(name, brand, colors, dailyRentalPrice, launchedAt, null, null);
 	}
 
 	public ProductBuilder withName(String name) {
@@ -39,11 +38,6 @@ public class ProductBuilder {
 
 	public ProductBuilder withBrand(Brand brand) {
 		this.brand = brand;
-		return this;
-	}
-
-	public ProductBuilder withModel(String model) {
-		this.model = model;
 		return this;
 	}
 
@@ -57,7 +51,7 @@ public class ProductBuilder {
 		return this;
 	}
 
-	public ProductBuilder withLaunchedAt(LocalDateTime launchedAt) {
+	public ProductBuilder withLaunchedAt(LocalDate launchedAt) {
 		this.launchedAt = launchedAt;
 		return this;
 	}
