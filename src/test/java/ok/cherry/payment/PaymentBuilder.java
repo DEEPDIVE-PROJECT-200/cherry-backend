ce9f7a76facebfe79bbf1a90c5eb4ebb8dbeebfd
Ogu1208 <kdasunb6@gmail.com>
2025-09-08 03:35:24 +0900

    test: Payment 도메인, 서비스 단위테스트 작성 (#71)

--- src/test/java/ok/cherry/payment/PaymentBuilder.java
+++ src/test/java/ok/cherry/payment/PaymentBuilder.java
@@ -1,5 +1,7 @@
 package ok.cherry.payment;
 
+import java.math.BigDecimal;
+
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import ok.cherry.member.MemberBuilder;
@@ -15,6 +17,8 @@ public class PaymentBuilder {
 	public Member member = MemberBuilder.create();
 	public Rental rental = RentalBuilder.builder().withMember(member).build();
 	public PaymentMethod paymentMethod = PaymentMethod.KAKAO_PAY;
+	public BigDecimal shippingFee = BigDecimal.valueOf(3000);
+	public BigDecimal cleaningFee = BigDecimal.valueOf(2000);
 
 	private boolean rentalExplicitlySet = false;
 
@@ -27,7 +31,7 @@ public class PaymentBuilder {
 	}
 
 	public Payment build() {
-		return Payment.create(member, rental, paymentMethod);
+		return Payment.create(member, rental, paymentMethod, shippingFee, cleaningFee);
 	}
 
 	public PaymentBuilder withMember(Member member) {
@@ -46,6 +50,22 @@ public class PaymentBuilder {
 
 	public PaymentBuilder withPaymentMethod(PaymentMethod paymentMethod) {
 		this.paymentMethod = paymentMethod;
+		return this;
+	}
+
+	public PaymentBuilder withShippingFee(BigDecimal shippingFee) {
+		this.shippingFee = shippingFee;
+		return this;
+	}
+
+	public PaymentBuilder withCleaningFee(BigDecimal cleaningFee) {
+		this.cleaningFee = cleaningFee;
+		return this;
+	}
+
+	public PaymentBuilder withAdditionalFee(BigDecimal shippingFee, BigDecimal cleaningFee) {
+		this.shippingFee = shippingFee;
+		this.cleaningFee = cleaningFee;
 		return this;
 	}
 }
