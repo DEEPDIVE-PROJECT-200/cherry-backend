d711f66dd79bcfce6faf8432c45f4ebbb534d4ff
chanwon <clw8679@naver.com>
2025-09-08 17:27:40 +0900

    feat: admin 초기 데이터 세팅 기능 구현 (#89)

---
+++ src/main/java/ok/cherry/admin/application/AdminDataInitializer.java
@@ -0,0 +1,28 @@
+package ok.cherry.admin.application;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import ok.cherry.admin.domain.Admin;
+import ok.cherry.admin.infrastructure.AdminRepository;
+
+@Configuration
+@RequiredArgsConstructor
+@Transactional
+@Profile("local")
+public class AdminDataInitializer implements CommandLineRunner {
+
+	private final AdminRepository adminRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	@Override
+	public void run(String... args) {
+		if (adminRepository.findByUsername("admin").isEmpty()) {
+			adminRepository.save(new Admin("admin", passwordEncoder.encode("admin")));
+		}
+	}
+}
