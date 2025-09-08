697d2919a4148c3fe69b9a7a78c3a48de60ada72
Jimin730 <dnddnd120@naver.com>
2025-09-08 16:14:00 +0900

    feat: 상품 삭제 로직 구현 (#96)

---
+++ src/main/java/ok/cherry/product/application/ProductApplicationService.java
@@ -0,0 +1,39 @@
+package ok.cherry.product.application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import ok.cherry.global.exception.error.BusinessException;
+import ok.cherry.global.s3.S3Service;
+import ok.cherry.product.domain.Product;
+import ok.cherry.product.exception.ProductError;
+import ok.cherry.product.infrastructure.ProductRepository;
+
+@Service
+@RequiredArgsConstructor
+public class ProductApplicationService {
+
+	private final ProductRepository productRepository;
+	private final S3Service s3Service;
+
+	public void deleteProduct(Long productId) {
+		Product product = productRepository.findById(productId)
+			.orElseThrow(() -> new BusinessException(ProductError.PRODUCT_NOT_FOUND));
+
+		List<String> imagePrefix = new ArrayList<>();
+		product.getDetail().getProductThumbnailDetails()
+			.forEach(detail -> imagePrefix.add(detail.getImageUrl()));
+
+		product.getDetail().getProductImageDetails()
+			.forEach(detail -> imagePrefix.add(detail.getImageUrl()));
+
+		if (!imagePrefix.isEmpty()) {
+			s3Service.deleteFiles(imagePrefix);
+		}
+
+		productRepository.delete(product);
+	}
+}
