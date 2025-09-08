6fca7a68f0e466c58cffc8cad8f503c8d8063eb9
Ogu1208 <kdasunb6@gmail.com>
2025-09-09 04:30:08 +0900

    refactor: ShippingController에서 ShippingSertice를 의존하도록 수정 (#94)

--- src/main/java/ok/cherry/shipping/presentation/ShippingController.java
+++ src/main/java/ok/cherry/shipping/presentation/ShippingController.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import ok.cherry.shipping.infrastructure.ShippingRepository;
+import ok.cherry.shipping.application.ShippingService;
 
 @RestController
 @RequestMapping("/api/v1/shipping")
 @RequiredArgsConstructor
 public class ShippingController {
 
-	private final ShippingRepository shippingRepository;
+	private final ShippingService shippingService;
 }
