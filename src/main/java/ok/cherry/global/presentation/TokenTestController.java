93db5363ef0b9ad276bed2729108ff05281f4fc4
chanwon <clw8679@naver.com>
2025-09-08 23:50:53 +0900

    refactor: 토큰 발급 컨트롤러 로그인 로직 변경 (#90)

--- src/main/java/ok/cherry/global/presentation/TokenTestController.java
+++ src/main/java/ok/cherry/global/presentation/TokenTestController.java
@@ -7,32 +7,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ok.cherry.auth.application.AuthService;
 import ok.cherry.auth.application.dto.response.TokenResponse;
-import ok.cherry.auth.jwt.TokenGenerator;
-import ok.cherry.global.exception.error.BusinessException;
-import ok.cherry.global.swagger.auth.TokenTestControllerDoc;
-import ok.cherry.member.domain.Member;
-import ok.cherry.member.exception.MemberError;
-import ok.cherry.member.infrastructure.MemberRepository;
+import ok.cherry.auth.util.CookieManager;
 
 @RestController
 @Slf4j
 @RequestMapping("/test")
 @RequiredArgsConstructor
 @Profile("local")
-public class TokenTestController implements TokenTestControllerDoc {
+public class TokenTestController {
 
-	private final TokenGenerator tokenGenerator;
-	private final MemberRepository memberRepository;
+	private final AuthService authService;
+	private final CookieManager cookieManager;
 
 	@PostMapping("/token/{providerId}")
-	public ResponseEntity<TokenResponse> generateAccessTokenForTest(@PathVariable String providerId) {
-		Member member = memberRepository.findByProviderId(providerId)
-			.orElseThrow(() -> new BusinessException(MemberError.USER_NOT_FOUND));
-
-		TokenResponse tokenResponse = tokenGenerator.generateTokenDTO(member);
+	public ResponseEntity<TokenResponse> generateAccessTokenForTest(
+		HttpServletResponse response,
+		@PathVariable String providerId
+	) {
+		TokenResponse tokenResponse = authService.login(providerId);
+		cookieManager.setCookie(response, "refreshToken", tokenResponse.refreshToken());
 		log.info("token generated: {}", tokenResponse);
 		return ResponseEntity.ok(tokenResponse);
 	}
