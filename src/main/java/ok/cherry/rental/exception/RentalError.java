6c154cadc58a1bc05970ba75f1567ec096858965
chanwon <clw8679@naver.com>
2025-09-10 00:19:51 +0900

    feat: 배송 시작, 완료 로직 구현 (#103)

--- src/main/java/ok/cherry/rental/exception/RentalError.java
+++ src/main/java/ok/cherry/rental/exception/RentalError.java
@@ -14,7 +14,9 @@ public enum RentalError implements ErrorCode {
 	RENTAL_ITEMS_NOT_EMPTY("대여 상품은 비어있을 수 없습니다", HttpStatus.BAD_REQUEST, "R_002"),
 	INVALID_RENTAL_PERIOD("대여 시작일이 종료일보다 늦을 수 없습니다", HttpStatus.BAD_REQUEST, "R_003"),
 	INVALID_RENTAL_REQUEST("유효하지 않은 대여 요청입니다", HttpStatus.BAD_REQUEST, "R_004"),
-	AMBIGUOUS_RENTAL_REQUEST("단건 대여 주문과 장바구니 주문을 동시에 요청할 수 없습니다", HttpStatus.BAD_REQUEST, "R_005");
+	AMBIGUOUS_RENTAL_REQUEST("단건 대여 주문과 장바구니 주문을 동시에 요청할 수 없습니다", HttpStatus.BAD_REQUEST, "R_005"),
+	NOT_PENDING("대여 준비 상태가 아닙니다", HttpStatus.BAD_REQUEST, "R_006"),
+	RENTAL_NOT_FOUND("대여를 찾을 수 없습니다", HttpStatus.NOT_FOUND, "R_007");
 
 	private final String message;
 	private final HttpStatus status;
--- src/main/java/ok/cherry/shipping/application/ShippingService.java
+++ src/main/java/ok/cherry/shipping/application/ShippingService.java
@@ -5,10 +5,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ok.cherry.global.exception.error.BusinessException;
 import ok.cherry.member.domain.Member;
 import ok.cherry.rental.domain.Rental;
 import ok.cherry.shipping.application.command.CreateShippingCommand;
 import ok.cherry.shipping.domain.Shipping;
+import ok.cherry.shipping.domain.type.Direction;
+import ok.cherry.shipping.exception.ShippingError;
 import ok.cherry.shipping.infrastructure.ShippingRepository;
 import ok.cherry.shipping.util.TrackingNumberGenerator;
 
@@ -38,6 +41,26 @@ public class ShippingService {
 		callExternalShippingGateway(shipping);
 
 		return shippingRepository.save(shipping);
+	}
+
+	@Transactional
+	public void startShipping(Long shippingId) {
+		Shipping shipping = shippingRepository.findById(shippingId)
+			.orElseThrow(() -> new BusinessException(ShippingError.SHIPPING_NOT_FOUND));
+		shipping.startShipping();
+		log.info("배송 시작 - 운송장 번호:{}, 배송 시작 시간: {}", shipping.getTrackingNumber(), shipping.getDetail().getStartAt());
+	}
+
+	@Transactional
+	public void completeShipping(Long shippingId) {
+		Shipping shipping = shippingRepository.findById(shippingId)
+			.orElseThrow(() -> new BusinessException(ShippingError.SHIPPING_NOT_FOUND));
+
+		if (shipping.getDirection() == Direction.OUTBOUND) {
+			shipping.getRental().active();
+		}
+		shipping.completeShipping();
+		log.info("배송 완료 - 운송장 번호:{}, 배송 완료 시간: {}", shipping.getTrackingNumber(), shipping.getDetail().getEndAt());
 	}
 
 	/**
