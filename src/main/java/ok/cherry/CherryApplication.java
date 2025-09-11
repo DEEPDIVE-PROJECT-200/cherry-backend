634df31c96390cbd7337580634047b6804defd97
chanwon <clw8679@naver.com>
2025-09-11 12:15:05 +0900

    chore: cors 설정 추가 (#146)

--- src/main/java/ok/cherry/CherryApplication.java
+++ src/main/java/ok/cherry/CherryApplication.java
@@ -3,9 +3,11 @@ package ok.cherry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class CherryApplication {
 
 	public static void main(String[] args) {
