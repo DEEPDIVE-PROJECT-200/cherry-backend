7d27ecc90276a16286f876f9c41fd758bd83fe9c
chanwon <clw8679@naver.com>
2025-09-11 12:20:38 +0900

    fix: debug 출력 기능 어노테이션 수정 (#146)

--- src/main/java/ok/cherry/CherryApplication.java
+++ src/main/java/ok/cherry/CherryApplication.java
@@ -3,11 +3,9 @@ package ok.cherry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableScheduling
 @SpringBootApplication
-@EnableWebSecurity(debug = true)
 public class CherryApplication {
 
 	public static void main(String[] args) {
